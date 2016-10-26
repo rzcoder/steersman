b51d2a5bbe18fe4bfbfc34ed74447bbeed0a2f38
rzcoder <rzcoder@gmail.com>
2016-10-27 01:45:44 +0500

    mistype

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
-const NORMALIZE_PATYH_REGEXP = /^[\s\/]*([^\s]*?)[\s\/]*$/i;
+const NORMALIZE_PATH_REGEXP = /^[\s\/]*([^\s]*?)[\s\/]*$/i;
 
 class InvalidPathError extends Error {
 }
 
 export function normalizePath(path: string): string {
-    const match = path.match(NORMALIZE_PATYH_REGEXP);
+    const match = path.match(NORMALIZE_PATH_REGEXP);
     if (typeof path !== "string" || !match) {
         throw new InvalidPathError("Invalid path string");
     }
