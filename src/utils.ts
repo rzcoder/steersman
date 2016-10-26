54c22326abf078fe6a45827b870bbda9958b822b
rzcoder <rzcoder@gmail.com>
2016-10-27 01:46:49 +0500

    mistype

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,8 @@ class InvalidPathError extends Error {
 }
 
 export function normalizePath(path: string): string {
-    const match = path.match(NORMALIZE_PATH_REGEXP);
-    if (typeof path !== "string" || !match) {
+    let match;
+    if (typeof path !== "string" || !(match = path.match(NORMALIZE_PATH_REGEXP))) {
         throw new InvalidPathError("Invalid path string");
     }
     return match[1];
