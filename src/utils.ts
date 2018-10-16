cf8078744260a7d7289d182273c129fb2993413d
rzcoder <rzcoder@gmail.com>
2018-10-16 13:12:42 +0500

    version bump

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ class InvalidPathError extends Error {
 export function normalizePath(path: string): string {
     let match;
     if (typeof path !== "string" || !(match = path.match(NORMALIZE_PATH_REGEXP))) {
-        throw new InvalidPathError("Invalid path string");
+        throw new InvalidPathError("Invalid path string " + path);
     }
     return match[1];
 }
